1948d6f771c968d584b5994f3cf1b2d8de9261e8
Mel1ssa <melissa.mekaoui@gmail.com>
2018-08-25 01:10:29 +0200

    adding services section + updating contact & about us sections

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,12 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contactAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/contact.html.twig');
+    }
 }
